c58e278f014c980288bb73d5106e00ef111b7d5c
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-10 12:01:50 +0530

    Add files via upload

---
+++ ArrayString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArrayString
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a[]={"apple","orange","Pine apple"};
+        for(String b: a)
+        System.out.print(b+",");
+        
+    }
+}
